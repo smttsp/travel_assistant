8e8bd7cd24722824e5c97cbbc1478cc518fd002d
Samet <samettaspinarx@gmail.com>
2023-06-01 11:58:58 -0400

    Initial commit

---
+++ example_template/__init__.py
@@ -0,0 +1,5 @@
+"""The `example_template` Python Package
+
+This is an example Python package for use internally at Overjet by developers looking to build out
+new Python packages, to isolate and share code.
+"""
